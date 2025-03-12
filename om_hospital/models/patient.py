0bb4c602b13227c9eb27028fcfa61c8a94d7a98b
Rakib48-Code <shahidulhuda0@gmail.com>
2025-03-12 10:48:42 +0600

    create compute field

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from datetime import date
 
 
 class HospitalPatient(models.Model):
@@ -7,8 +8,18 @@ class HospitalPatient(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
     name = fields.Char(string='Name', tracking=True)
-    age = fields.Integer(string='Age', tracking=True)
+    age = fields.Integer(string='Age', compute='_compute_age')
     gender = fields.Selection([('male','Male'),
                                ('female','Female'),
                                ], string='Gender', tracking=True)
     date_of_birth = fields.Date(string='Date of Birth', tracking=True)
+
+
+    @api.onchange('date_of_birth')
+    def _compute_age(self):
+        for rec in self:
+            today = date.today()
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 0
